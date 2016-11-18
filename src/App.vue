8bb1befc69d6ce4eb23b9961eb042a51013bc3ed
gaoge <gaoge@wecash.net>
2016-11-18 15:29:03 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+      <el-row :gutter="20">
+        <el-col :span="6"><div class="mydiv">1</div></el-col>
+        <el-col :span="6"><div class="mydiv">1</div></el-col>
+        <el-col :span="6"><div class="mydiv">1</div></el-col>
+        <el-col :span="6"><div class="mydiv">1</div></el-col>
+      </el-row>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.mydiv {
+  background-color: #fc9;
+}
+</style>
